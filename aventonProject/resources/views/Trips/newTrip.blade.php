dda5e0f79b42f5cf2a49c4d57610d05c3f714877
Alexis Torrena <alexis.torrena@outlook.com>
2018-06-01 01:20:30 -0300

    Validacion de Nuevo Viaje
    validacion completada, echa en el lado del cliente.

--- aventonProject/resources/views/Trips/newTrip.blade.php
+++ aventonProject/resources/views/Trips/newTrip.blade.php
@@ -9,40 +9,55 @@
           </button>
         </div>
         <div class="modal-body">
-            <form role="form" method="POST" action="/Trips">
+            <form class="needs-validation" role="form" method="POST" action="/Trips" novalidate>
               @csrf
                 <div class="form-group">
                   <label for="origin">Origen:</label>
-                  <input type="text" class="form-control" id="origin" name="origin">
+                  <input type="text" class="form-control" id="origin" name="origin" required>
+                  <div class="invalid-feedback">
+                      Por Favor ingrese un Origen.
+                  </div>
                 </div>
                 <div class="form-group">
                   <label for="destination">Destino:</label>
-                  <input type="text" class="form-control" id="destination" name="destination">
+                  <input type="text" class="form-control" id="destination" name="destination" required>
+                  <div class="invalid-feedback">
+                      Por Favor ingrese un Destino.
+                  </div>
                 </div>
                 <div class="form-group">
                     <label for="dateTime">Fecha y Hora:</label>
                     <div class='input-group date'>
-                        <input id='datetimepicker1' type='text' class="form-control" name="datetime" />
-                    </div>
+                        <input id='datetimepicker1' type='text' class="form-control" name="datetime" required/>
+                        <div class="invalid-feedback">
+                            Por Favor ingrese una Fecha y Hora.
+                        </div>
+                      </div>
                 </div>
                 <div class="form-group">
                     <label for="cost">Costo:</label>
-                    <input type="text" class="form-control" id="cost" name="cost">
-                  </div>
-                  <div class="form-group dropdown">
-                      <label for="freq">Frecuencia:</label>
-                      <input type="hidden" name="periodicity" value="" id='periodicity'>
-                      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span id="selected">Elige</span
-                      <span class="caret"></span></button>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Unica</a></li>
-                        <li><a class="dropdown-item" href="#">Diaria</a></li>
-                        <li><a class="dropdown-item" href="#">Semanal</a></li>
-                        <li><a class="dropdown-item" href="#">Mensual</a></li>
-                      </ul>
+                    <input type="number" class="form-control" id="cost" min="1" name="cost" required>
+                    <div class="invalid-feedback">
+                        Por Favor ingrese un Costo Mayor a 0.
                     </div>
+                </div>
+                <div class="container">
+                  <div class="row">
+                    <div class="form-group dropdown">
+                      <label for="freq">Frecuencia:</label>
+                      <select class="btn btn-primary dropdown-toggle custom-select" required name="periodicity">
+                        <option value=""><span class="caret">Elige una!</option>
+                        <option value="Unica">Unica</option>
+                        <option value="Diaria">Diaria</option>
+                        <option value="Semanal">Semanal</option>
+                        <option value="Semanal">Mensual</option>
+                      </select>
+                      <div class="invalid-feedback">Debe seleccionar una frecuencia</div>
+                    </div>
+                  </div>
+                </div> 
                   <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="reset();">Cerrar</button>
                     <button type="submit" class="btn btn-primary">Guardar</button>
                   </div>
               </form>
