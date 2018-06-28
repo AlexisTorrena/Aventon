3fb66c16ac2c515aef05d63af00185bbf5b43b00
Franco Tagliero <ftagliero@gmail.com>
2018-06-28 14:55:49 -0300

    verperfil

--- aventonProject/app/User.php
+++ aventonProject/app/User.php
@@ -26,4 +26,15 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getNameAtributte($value='')
+    {
+      return \Auth::user()->name;
+    }
+
+    public function getBirthDateAtributte($value='')
+    {
+      return \Auth::user()->birthDate;
+    }
+
 }
--- aventonProject/database/seeds/DatabaseSeeder.php
+++ aventonProject/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TripConfigurationsSeeder::class,
             TripsTableSeeder::class,
+            VehicleTableSeeder::class,
         ]);
     }
 }
