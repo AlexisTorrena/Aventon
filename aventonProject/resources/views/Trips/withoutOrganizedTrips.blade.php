26d9dce70cac4142022942dfbcaab4896a187d94
Alexis Torrena Solis <alexis.torrena@outlook.com>
2018-07-15 21:56:16 -0300

    bug fixing on my organized trips
    bug fixing on my organized trips

--- aventonProject/resources/views/Trips/withoutOrganizedTrips.blade.php
+++ aventonProject/resources/views/Trips/withoutOrganizedTrips.blade.php
@@ -6,7 +6,7 @@
     <h4 class="alert-heading">No posee Viajes!</h4>
     <p>Usted no posee viajes organizados en el sistema por favor agregue uno</p>
     <hr>
-    <a href="registerTrip" class="btn btn-outline-danger">Dar Aventon!</a>
+    <a href="/registerTrip" class="btn btn-outline-danger">Dar Aventon!</a>
   </div>
 </div>
 @endsection
